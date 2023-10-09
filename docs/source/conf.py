40fc9bbda51178a5ced84f13846d5c2b67366bf6
Jason Friedman <jasonfriedman27@gmail.com>
2023-10-09 12:31:10 -0400

    jason removed his conf.py edits, previous attempted fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,13 +4,13 @@
 
 # Jason added this 3pm 9/29/23 little block to debug broken images
 # https://github.com/readthedocs/readthedocs.org/issues/1846#issuecomment-477184259
-import os
-if not os.path.exists('./git-lfs'):
-    os.system('wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.1/git-lfs-linux-amd64-v2.7.1.tar.gz')
-    os.system('tar xvfz git-lfs-linux-amd64-v2.7.1.tar.gz')
-    os.system('./git-lfs install')  # make lfs available in current repository
-    os.system('./git-lfs fetch')  # download content from remote
-    os.system('./git-lfs checkout')  # make local files to have the real content on them
+# import os
+# if not os.path.exists('./git-lfs'):
+#     os.system('wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.1/git-lfs-linux-amd64-v2.7.1.tar.gz')
+#     os.system('tar xvfz git-lfs-linux-amd64-v2.7.1.tar.gz')
+#     os.system('./git-lfs install')  # make lfs available in current repository
+#     os.system('./git-lfs fetch')  # download content from remote
+#     os.system('./git-lfs checkout')  # make local files to have the real content on them
 
 project = 'Go-Kart'
 copyright = '2023, XLAB '
